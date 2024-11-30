63570e020dec3382d03ffa948d3859db642805ed
MagicTheG <mahir.mshahriar@gmail.com>
2024-11-29 23:23:08 -0500

    Update Uhhh.py

--- Uhhh.py
+++ Uhhh.py
@@ -1,5 +1,5 @@
 import requests
-category = 'money'
+category = 'inspirational'
 api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
 response = requests.get(api_url, headers={'X-Api-Key': 'cY5eb2ZW0+vU4L4T0l2Jmg==YzmNfsxnguZmjgAD'})
 if response.status_code == requests.codes.ok:
