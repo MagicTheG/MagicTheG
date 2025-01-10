6d4fecf261a069c4bf25926f44fa8d223a59f7a2
MagicTheG <156554303+MagicTheG@users.noreply.github.com>
2025-01-10 16:58:04 -0500

    Delete Uhhh.py

--- Uhhh.py
+++
@@ -1,8 +0,0 @@
-import requests
-category = 'inspirational'
-api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
-response = requests.get(api_url, headers={'X-Api-Key': 'cY5eb2ZW0+vU4L4T0l2Jmg==YzmNfsxnguZmjgAD'})
-if response.status_code == requests.codes.ok:
-    print(response.text)
-else:
-    print("Error:", response.status_code, response.text)
